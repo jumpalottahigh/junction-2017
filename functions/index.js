1530506bf6002fcaafe7ea93ee6c438b2530b89b
Antti Pyykkönen <antti.pyykkonen@zef.fi>
2017-11-25 21:47:10 +0200

    Handling uploading images

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+    databaseURL: "https://junction-2017.firebaseio.com/",
+    storageBucket: "junction-2017.appspot.com",
+    projectId: "junction-2017"
+});
 
 exports.handleImageUpload = functions.database.ref('/uploads/{uploadKey}')
     .onCreate(event => {
         // TODO: HANDLE UPLOAD STUFF
+        // 1. Firestore connect
+        const firestore = admin.firestore();
+        console.log(firestore);
+        const timestamp = new Date().getTime();
+        firestore.collection('items').add({
+            depositDate: timestamp,
+            name: 'kissa' 
+        }); //  TODO: GET NAME GOOGLE VISION
+
+
         const data = event.data.val();
         console.log('image uploaded url:', data.url);
     });
